d75b425d4be66c5c96de24d0d1469f2443ce52d8
Sean Clemmer <sczizzo@gmail.com>
2011-09-04 18:56:15 -0500

    Playing around with 1140 Grid, TimeAgo; adding documentation

--- javascripts/chicago-app.js
+++ javascripts/chicago-app.js
@@ -1,3 +1,6 @@
 head.ready(function() {
   // All of our JavaScript libraries are loaded! Let's get to work...
+
+  // TimeAgo coverts a specially formatted tag into an automagically-updating timestamp
+  $('.time-ago').timeago();
 });
