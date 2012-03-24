abde24d18463c1d357fdc542b5e8477803a4cde9
Sean Clemmer <sczizzo@gmail.com>
2012-03-23 19:40:32 -0500

    Updated copy; added tagline, sponsors, annoucement signup; unhid repos and colophon

--- javascripts/hack-uchicago.js
+++ javascripts/hack-uchicago.js
@@ -1,3 +1,29 @@
 head.ready(function() {
-  // All of our JavaScript libraries are loaded! Let's get to work...
+  var mailchimp_form = $('form#mc-embedded-subscribe-form');
+  mailchimp_form.find('.inside_label').each(function() {
+    var input = $(this).find('input');
+    var label = $(this).find('label');
+    input.data('default_text', label.text());
+
+    input.focus(function() {
+      if ( $(this).val() === $(this).data('default_text') ) {
+        $(this).removeClass();
+        $(this).val('');
+      }
+    });
+
+    input.blur(function() {
+      if ( $(this).val() === '' ) {
+        $(this).addClass('undisturbed');
+        $(this).val( $(this).data('default_text') );
+      }
+    });
+
+    mailchimp_form.submit(function() {
+      input.focus();
+    });
+
+    label.hide();
+    input.blur();
+  });
 });
